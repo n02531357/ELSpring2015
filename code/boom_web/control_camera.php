1ba4332dd9dcdcec1003d5af68ed6b1dac320288
Charles Palsa <n02531357@hawkmail.newpaltz.edu>
2015-05-01 16:28:37 -0400

    Axis pictures, gallery work.

--- code/boom_web/control_camera.php
+++ code/boom_web/control_camera.php
@@ -1,47 +1,56 @@
 <?php
 
-$motor_script_location = "/home/pi/Adafruit-Raspberry-Pi-Python-Code/Adafruit_PWM_Servo_Driver/test_WriteFile.py";
-$m1 = 0;
-$m2 = 0;
-$m3 = 0;
+// $teste = fopen("newfile.txt", "w") or die("Unable to open file!");
+// $txt = "John Doe\n";
+// fwrite($teste, $txt);
+// fclose($teste);
+
+ $motor1 = '0';
+ $motor2 = '0';
+ $motor3 = '0';
+
+$motor_script_location = "./Adafruit_PWM_Servo_Driver/test_WriteFile.py";
+
+$handle = fopen("./Adafruit_PWM_Servo_Driver/Location_Motor", "r") or die("Unable to open file!");
+if ($handle)
+{
+ $motor1 = fgets($handle);
+ $motor2 = fgets($handle);
+ $motor3 = fgets($handle);	
+}
+fclose($handle);
 
 $direction = $_POST['direction'];
 
-if ($direction == 'xup')
+ $m1 = intval($motor1);
+ $m2 = intval($motor2);
+ $m3 = intval($motor3);	
+
+$angle = 5;
+
+if (strcmp($direction, 'xup') == 0)
 {
-	$m1 = 0;
-	$m2 = 0;
-	$m3 = 0;
+	$m1 = $m1 + $angle;
 }
-else if ($direction == 'xdown')
+else if (strcmp($direction, 'xdown') == 0)
 {
-	$m1 = 0;
-	$m2 = 0;
-	$m3 = 0;
+	$m1 = $m1 - $angle;
 }
-else if ($direction == 'yup')
+else if (strcmp($direction, 'yup') == 0)
 {
-	$m1 = 0;
-	$m2 = 0;
-	$m3 = 0;
+	$m2 = $m2 + $angle;
 }
-else if ($direction == 'ydown')
+else if (strcmp($direction, 'ydown') == 0)
 {
-	$m1 = 0;
-	$m2 = 0;
-	$m3 = 0;
+	$m2  = $m2 - $angle;
 }
-else if ($direction == 'zup')
+else if (strcmp($direction, 'zup') == 0)
 {
-	$m1 = 0;
-	$m2 = 0;
-	$m3 = 0;
+	$m3 = $m3 + $angle;
 }
-else if ($direction == 'zdown')
+else if (strcmp($direction, 'zdown') == 0)
 {
-	$m1 = 0;
-	$m2 = 0;
-	$m3 = 0;
+	$m3 = $m3 - $angle;
 }
 else 
 {
@@ -51,7 +60,7 @@ else
 	$m3 = 0;
 }
 
-$cmd = "python ".$motor_script_location." ".$m1." ".$m2." ".$m3;
+$cmd = "sudo python ".$motor_script_location." ".$m1." ".$m2." ".$m3;
 $output = array();
 exec($cmd, $output);
 
